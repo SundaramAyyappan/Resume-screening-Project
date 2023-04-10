05f42641b05680bb35672972c7a76b020aa79ef1
SundaramAyyappan <124044345+SundaramAyyappan@users.noreply.github.com>
2023-04-10 19:23:24 +0530

    Update resume_app.py

--- resume_app.py
+++ resume_app.py
@@ -5,14 +5,41 @@ Created on Mon Mar  6 12:44:35 2023
 @author: Jeevika
 """
 
-import numpy as np
-import pickle
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-import streamlit as st 
-
+import numpy as np
+import streamlit as st
+from io import BytesIO
+import click
+import spacy
 import docx2txt
-
+import pdfplumber
+from pickle import load
+import requests
+import re
+import os
+import sklearn
+import PyPDF2
+import nltk
+import pickle
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('omw-1.4')
+# load pre-trained model
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+from nltk.tokenize import RegexpTokenizer
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import matplotlib.pyplot  as plt
+stop=set(stopwords.words('english'))
+from spacy.matcher import Matcher
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
 
 from PIL import Image
 
@@ -30,62 +57,145 @@ def welcome():
 # defining the function which will make the prediction using 
 # the data which the user inputs
 
-def prediction(Skills):  
+def extract_skills(resume_text):
 
-    r = classifier.predict(loaded_vectorizer.transform([Skills]))
-    print(r)
-    return r
+    nlp_text = nlp(resume_text)
+    noun_chunks = nlp_text.noun_chunks
 
-# this is the main function in which we define our webpage
+    # removing stop words and implementing word tokenization
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+            
+    # reading the csv file
+    data = pd.read_csv("skills.csv") 
+            
+    # extract values
+    skills = list(data.columns.values)
+            
+    skillset = []
+            
+    # check for one-grams (example: python)
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+            
+    # check for bi-grams and tri-grams (example: machine learning)
+    for token in noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+            
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+# Function to extract text from resume
+def getText(filename):
+      
+    # Create empty string 
+    fullText = ''
+    if filename.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        doc = docx2txt.process(filename)
+        
+        for para in doc:
+            fullText = fullText + para
+            
+           
+    else:  
+        with pdfplumber.open(filename) as pdf_file:
+            pdoc = PyPDF2.PdfFileReader(filename)
+            number_of_pages = pdoc.getNumPages()
+            page = pdoc.pages[0]
+            page_content = page.extractText()
+             
+        for paragraph in page_content:
+            fullText =  fullText + paragraph
+         
+    return (fullText)
+
+def display(doc_file):
+    resume = []
+    if doc_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        resume.append(docx2txt.process(doc_file))
+
+    else:
+        with pdfplumber.open(doc_file) as pdf:
+            pages=pdf.pages[0]
+            resume.append(pages.extract_text())
+            
+    return resume
+
+def preprocess(sentence):
+    sentence=str(sentence)
+    sentence = sentence.lower()
+    sentence=sentence.replace('{html}',"") 
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', sentence)
+    rem_url=re.sub(r'http\S+', '',cleantext)
+    rem_num = re.sub('[0-9]+', '', rem_url)
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(rem_num)  
+    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    lemma_words=[lemmatizer.lemmatize(w) for w in filtered_words]
+    return " ".join(lemma_words)  
+
+
+# Function to extract experience details
+def expDetails(Text):
+    global sent
+   
+    Text = Text.split()
+   
+    for i in range(len(Text)-2):
+        Text[i].lower()
+        
+        if Text[i] ==  'years':
+            sent =  Text[i-2] + ' ' + Text[i-1] +' ' + Text[i] +' '+ Text[i+1] +' ' + Text[i+2]
+            l = re.findall(r'\d*\.?\d+',sent)
+            for i in l:
+                a = float(i)
+            return(a)
+            return (sent)
+
+target = {0:'Peoplesoft',1:'SQL Developer',2:'React JS Developer',3:'Workday'}
 
 def main():
-
-    # giving the webpage a title
-
-    st.title("Resume Screening")
-      
-    # here we define some of the front end elements of the web page like 
-    # the font and background color, the padding and the text to be displayed
-
-    html_temp = '''
-    <div style ="background-color:yellow;padding:13px">
-    <h1 style ="color:black;text-align:center;">Resume screening ML App </h1>
+    html_temp = """
+    <div style ="background-color:transparent;padding:13px">
+    <h1 style ="color:#576CBC;text-align:center;"> RESUME CLASSIFICATION </h1>
     </div>
-    '''
-      
-    # this line allows us to display the front end aspects we have 
-    # defined in the above code
-
+    """
     st.markdown(html_temp, unsafe_allow_html = True)
-      
-    # the following lines create text boxes in which the user can enter 
-    # the data required to make the prediction
 
-    st.subheader("DocumentFiles")
-    Skills = st.file_uploader("Upload Document", type = ["pdf","docx","doc","txt"]) 
+    file_type=pd.DataFrame([], columns=['Uploaded File', 'Experience', 'Skills', 'Predicted Profile'])
+    filename = []
+    predicted = []
+    experience = []
+    skills = []
     
+    upload_file = st.file_uploader('Hey,Upload Your Resumes ',
+                                type= ['docx','pdf'],accept_multiple_files=True)
 
-    
-    
- 
-       
-    result =""
-      
-    # the below line ensures that when the button called 'Predict' is clicked, 
-    # the prediction function defined above is called to make the prediction 
-    # and store it in the variable result
+    for doc_file in upload_file:
+            if doc_file is not None:
+                filename.append(doc_file.name)
+                cleaned=preprocess(display(doc_file))
+                prediction = classifier.predict(loaded_vectorizer.transform([cleaned]))[0]
+                predicted.append(target.get(prediction))
+                extText = getText(doc_file)
+                exp = expDetails(extText)
+                experience.append(exp)
+                skills.append(extract_skills(extText))
+    if len(predicted) > 0:
+        file_type['Uploaded File'] = filename
+        file_type['Experience'] = experience
+        file_type['Skills'] = skills
+        file_type['Predicted Profile'] = predicted
+        # file_type
+        # Custom formatting
+        st.table(file_type.style.format({'Experience': '{:.1f}'}))
 
-    if st.button("Process"):
-      if Skills is not None:
-        file_details = {"filename":Skills.name,
-                        "filetype":Skills.type,"filesize":Skills.size}
-        
-        raw_text = docx2txt.process(Skills)
-        
-        
-        
-        result = prediction(raw_text)
-    st.success('The output is {}'.format(result))
-
-if __name__=='__main__':
-    main()
+    st.write(f'*Note: Classifies only for Workday, Peoplesoft, React JS and SQL Developer Resumes')
+    st.subheader("About") 
+    st.info("This project is a part of AiVariant Internship")
+   
+if __name__ == '__main__':
+     main()
